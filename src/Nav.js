872bc98e091ed3808c7bb366c8465cc5746416fe
stackpointer1 <76005177+stackpointer1@users.noreply.github.com>
2021-03-04 02:00:06 +0530

    added react router in nav.js added react router in nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,19 @@
 import React from "react";
 import "./styles.css";
-import { Link, withRouter } from "react-router-dom";
+import { Link} from "react-router-dom";
 function Nav() {
   const navStyle = {
     color: "white"
   };
   return (
     <nav>
-      <h3>logo</h3>
+      <h3>AMAZON</h3>
       <ul classNmae="nav-links">
         <Link style={navStyle} to="/about">
           <li>About</li>
         </Link>
-        <Link>
-          <li style={navStyle} to="/shop">
-            Shop
-          </li>
+        <Link style={navStyle} to="/shop">
+          <li >Shop</li>
         </Link>
       </ul>
     </nav>
